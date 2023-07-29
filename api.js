735c3498efba7acd0e8b3542deab8ba53313a15b
Rashid <raabdulk@gmail.com>
2023-07-29 18:14:10 +0300

    Комментирую код

--- api.js
+++ api.js
@@ -1,10 +1,14 @@
-// Замени на свой, чтобы получить независимый от других набор данных.
+// Замени на свой, чтобы получить независимый от других набор данных. +
 // "боевая" версия инстапро лежит в ключе prod
-const personalKey = "prod";
-const baseHost = "https://webdev-hw-api.vercel.app";
-const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
-export function getPosts({ token }) {
+const personalKey = "rashid-abdulkhamidov";  // Персональный ключ разработчика
+const baseHost = `https://wedev-api.sky.pro/api/v1/${personalKey}/instapro`; // Посты пользователя 
+// https://wedev-api.sky.pro/api/v1/rashid-abdulkhamidov/instapro
+const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`; 
+// https://wedev-api.sky.pro/api/v1/rashid-abdulkhamidov/instapro/api/v1/rashid-abdulkhamidov/instapro
+
+
+export function getPosts({ token }) { // Получаем все посты пользователей
   return fetch(postsHost, {
     method: "GET",
     headers: {
@@ -24,7 +28,7 @@ export function getPosts({ token }) {
 }
 
 // https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
-export function registerUser({ login, password, name, imageUrl }) {
+export function registerUser({ login, password, name, imageUrl }) { // Регистрация пользователя
   return fetch(baseHost + "/api/user", {
     method: "POST",
     body: JSON.stringify({
@@ -41,7 +45,7 @@ export function registerUser({ login, password, name, imageUrl }) {
   });
 }
 
-export function loginUser({ login, password }) {
+export function loginUser({ login, password }) { // Авторизация пользователя
   return fetch(baseHost + "/api/user/login", {
     method: "POST",
     body: JSON.stringify({
